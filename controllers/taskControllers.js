9c41c40189f9711ebb5b3347dba84999dbfc1ab4
Success96 <lfawas6@gmail.com>
2023-11-06 16:53:49 +0100

    Error handling middlewares added

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,5 +1,6 @@
 const Tasks = require('../models/taskModel')
 const asyncWrapper = require('../middleware/asyncWrapper')
+const {createCustomError} = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper (async (req, res)=>{
     const tasks = await Tasks.find({})
@@ -11,11 +12,11 @@ const createTask = asyncWrapper(async (req, res)=>{
     res.status(201).json({ task })
 })
 
-const getTask = asyncWrapper (async (req, res)=>{
+const getTask = asyncWrapper (async (req, res, next)=>{
     const {id:taskID}= req.params
     const task = await Tasks.findOne({_id:taskID})
     if (!task){
-        res.status(404).json({msg: `No task with id ${taskID}`})
+        return next(createCustomError(`No task with id ${taskID}`, 404 ))
     }
     res.status(200).json({task})
 })
@@ -31,11 +32,11 @@ const updateTask = asyncWrapper (async (req, res)=>{
     res.status(200).json({task})}
 )
 
-const deleteTask = asyncWrapper(async (req, res)=>{
+const deleteTask = asyncWrapper(async (req, res, next)=>{
     const {id:taskID} = req.params
     const task = await Tasks.findOneAndDelete({_id:taskID})
     if (!task){
-        res.status(404).json({msg: `No task with id ${taskID}`})
+        return next(createCustomError(`No task with id ${taskID}`, 404 ))
     }
     res.status(200).json({msg: `${task.name} has been deleted`})}
 
--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,5 +1,10 @@
+const {CustomAPIError} = require('../errors/custom-error')
+
 const errorHandlerMiddleWare = (err, req, res, next) =>{
-    return res.status(500).json({msg: err})
+    if(err instanceof CustomAPIError){
+        return res.status(err.statusCode).json({msg: err.message})
+    }
+    return res.status(500).json({msg: 'Something went wrong, Please try again!'})
 }
 
 module.exports = errorHandlerMiddleWare
